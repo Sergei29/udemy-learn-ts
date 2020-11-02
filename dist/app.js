77ba486ec72ecd1a4a43b61db579e2bb61caa8de
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2020-11-02 18:23:59 +0000

    js new gen features

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,23 @@
 "use strict";
-const button = document.getElementById("button");
-function clickHandler(message) {
-    console.log("Clicked.", message);
-}
-button.addEventListener("click", clickHandler.bind(null, "Hello"));
-function sendAnalytics(msg) {
-    console.log(msg);
-}
-function add(n1, n2) {
-    if (n1 + n2 > 10) {
-        return n1 + n2;
-    }
-    return;
-}
+const add = (a, b = 1) => a + b;
+const printOutput = (result) => {
+    console.log("Result: ", result);
+};
+printOutput(add(5, 2));
+const addAll = (...numbers) => numbers.reduce((acc, current) => {
+    return (acc += current);
+}, 0);
+printOutput(addAll(1, 2, 3, 4, 5));
+const addOnlyThree = (...nums) => nums.reduce((acc, current) => (acc += current), 0);
+printOutput(addOnlyThree(1, 2, 3));
+const hobbies = ["Cooking", "Sports", "Singing", "Programming"];
+const person = {
+    firstName: "Serge",
+    age: 45,
+};
+const [hobby1, hobby2, ...otherHobbies] = hobbies;
+console.log("hobby1: ", hobby1);
+console.log("hobby2: ", hobby2);
+console.log("otherHobbies: ", otherHobbies);
+const { firstName, age } = person;
+console.log("firstName: ", firstName, ", age: ", age);
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,33 @@
-const button = document.getElementById("button")!;
+const add = (a: number, b: number = 1) => a + b;
 
-function clickHandler(message: string) {
-  console.log("Clicked.", message);
-}
+const printOutput = (result: string | number) => {
+  console.log("Result: ", result);
+};
 
-button.addEventListener("click", clickHandler.bind(null, "Hello"));
+printOutput(add(5, 2));
 
-function sendAnalytics(msg: string) {
-  console.log(msg);
-}
+const addAll = (...numbers: number[]) =>
+  numbers.reduce((acc, current) => {
+    return (acc += current);
+  }, 0);
 
-function add(n1: number, n2: number) {
-  if (n1 + n2 > 10) {
-    return n1 + n2;
-  }
-  return;
-}
+printOutput(addAll(1, 2, 3, 4, 5));
+
+const addOnlyThree = (...nums: [number, number, number]) =>
+  nums.reduce((acc, current) => (acc += current), 0);
+
+printOutput(addOnlyThree(1, 2, 3));
+
+const hobbies = ["Cooking", "Sports", "Singing", "Programming"];
+const person = {
+  firstName: "Serge",
+  age: 45,
+};
+
+const [hobby1, hobby2, ...otherHobbies] = hobbies;
+console.log("hobby1: ", hobby1);
+console.log("hobby2: ", hobby2);
+console.log("otherHobbies: ", otherHobbies);
+
+const { firstName, age } = person;
+console.log("firstName: ", firstName, ", age: ", age);
