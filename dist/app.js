77b4715f67c4a5e00417949763e2ca0cc29443bc
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2020-11-06 23:02:20 +0000

    classes-interfaces completed

--- dist/app.js
+++ dist/app.js
@@ -3,10 +3,20 @@ let user1;
 class Person {
     constructor(n) {
         this.age = 30;
-        this.name = n;
+        if (n) {
+            this.name = n;
+        }
+        else {
+            this.name = "Unknown";
+        }
     }
     greet(phrase) {
-        console.log(phrase);
+        console.log(phrase, " This is ", this.name);
     }
 }
 user1 = new Person("John");
+user1.greet("Hi!");
+const user2 = new Person();
+user2.greet("Hi there !");
+let add;
+add = (n1, n2) => n1 + n2;
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -9,16 +10,35 @@ interface Greetable extends Named {
 let user1: Greetable;
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    } else {
+      this.name = "Unknown";
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase);
+    console.log(phrase, " This is ", this.name);
   }
 }
 
 user1 = new Person("John");
+user1.greet("Hi!");
+const user2 = new Person();
+user2.greet("Hi there !");
+
+// interfaces as a function:
+
+// type AddFn = (a: number, b: number) => number;
+
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => n1 + n2;
