fc5792d3602e3a727bef1a50e92901b588c507d2
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-04-10 14:25:02 +0300

    validator deceorators

--- dist/app.js
+++ dist/app.js
@@ -5,116 +5,55 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-function Logger(logMessage) {
-    console.log("Logger Factory");
-    return function (constructor) {
-        console.log(logMessage);
-        console.log(`constructor: `, constructor);
-    };
+const registeredValidators = {};
+function RequiredField(target, propertyName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propertyName]: ["required"] });
 }
-function WithTemplate(template, hookId) {
-    console.log("Template Factory");
-    return function (originalConstructor) {
-        return class extends originalConstructor {
-            constructor(..._) {
-                super();
-                console.log("Rendering template...");
-                const hookEl = document.getElementById(hookId);
-                if (hookEl) {
-                    hookEl.innerHTML = template;
-                    hookEl.querySelector("h1").textContent = this.name;
-                }
+function PositiveNumber(target, propertyName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propertyName]: ["positive"] });
+}
+function validate(obj) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig)
+        return true;
+    let isValid = true;
+    for (const key in objValidatorConfig) {
+        for (const validator of objValidatorConfig[key]) {
+            switch (validator) {
+                case "required":
+                    isValid = isValid && obj[key].trim().length > 0;
+                    break;
+                case "positive":
+                    isValid = isValid && obj[key] > 0;
+                    break;
             }
-        };
-    };
-}
-let Person = class Person {
-    constructor() {
-        this.name = "Serge";
-        console.log("Creating Person object...");
+        }
     }
-};
-Person = __decorate([
-    Logger("\nLogging user..."),
-    WithTemplate("<h1>My Person Object.</h1>", "app")
-], Person);
-const pers = new Person();
-function Log(target, propertyName) {
-    console.log("\nProperty decorator");
-    console.log(target, propertyName);
+    return isValid;
 }
-function Log2(target, name, descriptor) {
-    console.log("\nAccessor decorator!");
-    console.log(target, name, descriptor);
-}
-function Log3(target, name, descriptor) {
-    console.log("\nMethod decorator!");
-    console.log(`target :>> `, target);
-    console.log("name :>> ", name);
-    console.log("descriptor :>> ", descriptor);
-}
-function Log4(target, methodName, argPosition) {
-    console.log("\nParameter decorator!");
-    console.log("target :>> ", target);
-    console.log("methodName :>> ", methodName);
-    console.log("argPosition :>> ", argPosition);
-}
-class Product {
+class Course {
     constructor(t, p) {
         this.title = t;
-        this._price = p;
-    }
-    set price(val) {
-        if (val > 0) {
-            this.price = val;
-        }
-        else {
-            throw new Error("Price must be positive.");
-        }
-    }
-    get price() {
-        return this._price;
-    }
-    getPriceWithTax(tax) {
-        return this.price * (1 + tax);
+        this.price = p;
     }
 }
 __decorate([
-    Log
-], Product.prototype, "title", void 0);
+    RequiredField
+], Course.prototype, "title", void 0);
 __decorate([
-    Log2
-], Product.prototype, "price", null);
-__decorate([
-    Log3,
-    __param(0, Log4)
-], Product.prototype, "getPriceWithTax", null);
-function Autobind(_, _2, descriptor) {
-    const originalMethod = descriptor.value;
-    const updatedDescriptor = {
-        configurable: true,
-        enumerable: false,
-        get() {
-            const boundFunc = originalMethod.bind(this);
-            return boundFunc;
-        },
-    };
-    return updatedDescriptor;
-}
-class Printer {
-    constructor() {
-        this.message = "This works!";
+    PositiveNumber
+], Course.prototype, "price", void 0);
+const courseForm = document.querySelector("form");
+courseForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const titleInput = document.getElementById("title");
+    const priceInput = document.getElementById("price");
+    const title = titleInput.value;
+    const price = +priceInput.value;
+    const createdCoure = new Course(title, price);
+    if (!validate(createdCoure)) {
+        alert("Form data is invalid. try again.");
+        return;
     }
-    showMesssage() {
-        console.log(this.message);
-    }
-}
-__decorate([
-    Autobind
-], Printer.prototype, "showMesssage", null);
-const printer = new Printer();
-const button = document.querySelector("button");
-button.addEventListener("click", printer.showMesssage);
+    console.log("createdCoure :>> ", createdCoure);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,162 +1,71 @@
-function Logger(logMessage: string) {
-  console.log("Logger Factory");
-
-  return function (constructor: Function) {
-    console.log(logMessage);
-    console.log(`constructor: `, constructor);
+// VALIDATION WITH DECORATORS
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProperty: string]: string[]; // ['required', 'positive', ...]
   };
 }
 
-/**
- * @description class decorator
- * @param {String} template template to insert
- * @param {String} hookId dom element ID
- * @returns {ClassDecorator} new enhanced class. And now all of a sudden we are able to add logic that does not run when the class is defined. But when the class is instantiated.
- */
-function WithTemplate(template: string, hookId: string) {
-  console.log("Template Factory");
+const registeredValidators: ValidatorConfig = {};
 
-  return function <T extends { new (...args: any[]): { name: string } }>(
-    originalConstructor: T
-  ) {
-    return class extends originalConstructor {
-      constructor(..._: any[]) {
-        super();
-        console.log("Rendering template...");
-        const hookEl = document.getElementById(hookId);
-        if (hookEl) {
-          hookEl.innerHTML = template;
-          hookEl.querySelector("h1")!.textContent = this.name;
-        }
+function RequiredField(target: any, propertyName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propertyName]: ["required"],
+  };
+}
+
+function PositiveNumber(target: any, propertyName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propertyName]: ["positive"],
+  };
+}
+
+function validate(obj: Record<string, any>) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) return true;
+  let isValid = true;
+  for (const key in objValidatorConfig) {
+    for (const validator of objValidatorConfig[key]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && obj[key].trim().length > 0;
+          break;
+        case "positive":
+          isValid = isValid && obj[key] > 0;
+          break;
       }
-    };
-  };
-}
-
-@Logger("\nLogging user...")
-@WithTemplate("<h1>My Person Object.</h1>", "app")
-class Person {
-  name = "Serge";
-  constructor() {
-    console.log("Creating Person object...");
-  }
-}
-
-const pers = new Person();
-// console.log(pers);
-
-// Property & Method decorators:
-
-/**
- * @description property decorator
- * @param {any} target object prototype or constructor method(if used for static)
- * @param {Sring} propertyName property name
- * @returns {undefined}
- */
-function Log(target: any, propertyName: string | Symbol) {
-  console.log("\nProperty decorator");
-  console.log(target, propertyName);
-}
-
-/**
- * @description accessor decorator - getters and setters
- * @param {any} target object prototype or constructor method(if used for static)
- * @param {String} name accessor name
- * @param {PropertyDescriptor} descriptor
- * @returns {any} can return a new `PropertyDescriptor` with updated values, which can upgrade the existing accessor
- */
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("\nAccessor decorator!");
-  console.log(target, name, descriptor);
-}
-
-/**
- * @description method decorator - class method
- * @param {any} target object prototype or constructor method(if used for static)
- * @param {String} name method name
- * @param {PropertyDescriptor} descriptor
- * @returns {any} can return a new `PropertyDescriptor` with updated values, which can upgrade the existing method or configuration of the method
- */
-function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("\nMethod decorator!");
-  console.log(`target :>> `, target);
-  console.log("name :>> ", name);
-  console.log("descriptor :>> ", descriptor);
-}
-
-/**
- * @description parameter, argument decorator
- * @param {any} target object prototype or constructor method(if used for static)
- * @param {String} methodName method name where the argumeht is passed
- * @param {Number} argPosition index position of the argument starting from 0
- * @returns {undefined}
- */
-function Log4(target: any, methodName: string, argPosition: number) {
-  console.log("\nParameter decorator!");
-  console.log("target :>> ", target);
-  console.log("methodName :>> ", methodName);
-  console.log("argPosition :>> ", argPosition);
-}
-
-class Product {
-  @Log
-  title: string;
-  private _price: number;
-
-  @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this.price = val;
-    } else {
-      throw new Error("Price must be positive.");
     }
   }
-
-  get price() {
-    return this._price;
-  }
-
+  return isValid;
+}
+class Course {
+  @RequiredField
+  title: string;
+  @PositiveNumber
+  price: number;
   constructor(t: string, p: number) {
     this.title = t;
-    this._price = p;
-  }
-
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
-    return this.price * (1 + tax);
+    this.price = p;
   }
 }
 
-/**
- * @description that's our decorator function returning a new descriptor object and therefore this descriptor object will override the old descriptor.
- */
-function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-  const originalMethod = descriptor.value;
+const courseForm = document.querySelector("form")!;
 
-  const updatedDescriptor: PropertyDescriptor = {
-    configurable: true,
-    enumerable: false,
-    get() {
-      const boundFunc = originalMethod.bind(this);
-      return boundFunc;
-    },
-  };
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleInput = document.getElementById("title") as HTMLInputElement;
+  const priceInput = document.getElementById("price") as HTMLInputElement;
 
-  return updatedDescriptor;
-}
+  const title = titleInput.value;
+  const price = +priceInput.value;
 
-class Printer {
-  message = "This works!";
+  const createdCoure = new Course(title, price);
 
-  @Autobind
-  showMesssage() {
-    console.log(this.message);
+  if (!validate(createdCoure)) {
+    alert("Form data is invalid. try again.");
+    return;
   }
-}
 
-const printer = new Printer();
-const button = document.querySelector("button")!;
-
-// button.addEventListener("click", printer.showMesssage.bind(p)); // if without Autobinder decorator
-
-button.addEventListener("click", printer.showMesssage);
+  console.log("createdCoure :>> ", createdCoure);
+});
