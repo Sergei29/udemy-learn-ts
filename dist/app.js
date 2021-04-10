8439a9cc45b9bd7a15ea218ce6615f7f58b0ff64
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-04-10 11:36:58 +0300

    decorators - class, accessor, property decorators

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,79 @@
 "use strict";
-class Person {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(logMessage) {
+    console.log("Logger Factory");
+    return function (constructor) {
+        console.log(logMessage);
+        console.log(`constructor: `, constructor);
+    };
+}
+function WithTemplate(template, hookId) {
+    console.log("Template Factory");
+    return function (constructor) {
+        console.log("Rendering template...");
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1").textContent = p.name;
+        }
+    };
+}
+let Person = class Person {
     constructor() {
         this.name = "Serge";
         console.log("Creating Person object...");
     }
+};
+Person = __decorate([
+    Logger("Logging user..."),
+    WithTemplate("<h1>My Person Object.</h1>", "app")
+], Person);
+function Log(target, propertyName) {
+    console.log("Property decorator");
+    console.log(target, propertyName);
 }
-const pers = new Person();
-console.log(pers);
+function Log2(target, name, descriptor) {
+    console.log("Accessor decorator!");
+    console.log(target, name, descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log("Method decorator!");
+    console.log(`target :>> `, target);
+    console.log("name :>> ", name);
+    console.log("descriptor :>> ", descriptor);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this.price = val;
+        }
+        else {
+            throw new Error("Price must be positive.");
+        }
+    }
+    get price() {
+        return this._price;
+    }
+    getPriceWithTax(tax) {
+        return this.price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3
+], Product.prototype, "getPriceWithTax", null);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,29 @@
+function Logger(logMessage: string) {
+  console.log("Logger Factory");
+
+  return function (constructor: Function) {
+    console.log(logMessage);
+    console.log(`constructor: `, constructor);
+  };
+}
+
+function WithTemplate(template: string, hookId: string) {
+  console.log("Template Factory");
+
+  return function (constructor: any) {
+    console.log("Rendering template...");
+
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+@Logger("Logging user...")
+@WithTemplate("<h1>My Person Object.</h1>", "app")
 class Person {
   name = "Serge";
   constructor() {
@@ -5,5 +31,53 @@ class Person {
   }
 }
 
-const pers = new Person();
-console.log(pers);
+// const pers = new Person();
+// console.log(pers);
+
+// Property & Method decorators:
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target, name, descriptor);
+}
+
+function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Method decorator!");
+  console.log(`target :>> `, target);
+  console.log("name :>> ", name);
+  console.log("descriptor :>> ", descriptor);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this.price = val;
+    } else {
+      throw new Error("Price must be positive.");
+    }
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(tax: number) {
+    return this.price * (1 + tax);
+  }
+}
